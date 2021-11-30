f65f1bf2676287f7a82fddaa3811d0ce1d08d278
sugarcothe <ifeanyivalentine82@gmail.com>
2021-11-30 11:44:49 +0000

    changed a green button to red button

---
+++ app.js
@@ -0,0 +1,8 @@
+let button = document.querySelector('button');
+let header = document.querySelector('h1');
+
+header.style.display = 'none'
+
+button.onclick = () => {
+    header.style.display = 'block'; 
+} 
